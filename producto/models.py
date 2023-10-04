57e1b9d80fe1e13bd0707d02d838c9cc275ae33a
hugounico <131213440+hugounico@users.noreply.github.com>
2023-10-04 19:13:53 -0300

    almost5

--- producto/models.py
+++ producto/models.py
@@ -4,7 +4,7 @@ from laboratorio.models import Laboratorio
 # Create your models here.
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
-    laboratorio = models.CharField(Lmax_length=100) #models.ForeignKey(Laboratorio)on_delete=models.CASCADE#Esta línea define el campo laboratorio en el modelo Producto como una clave externa (ForeignKey) que establece una relación muchos a uno con el modelo Laboratorio.
+    laboratorio = models.CharField(max_length=100) #models.ForeignKey(Laboratorio)on_delete=models.CASCADE#Esta línea define el campo laboratorio en el modelo Producto como una clave externa (ForeignKey) que establece una relación muchos a uno con el modelo Laboratorio.
     f_fabricacion = models.DateField(verbose_name="Fecha de fabricación", default='2015-01-01')
     p_costo = models.DecimalField(max_digits=10, decimal_places=2)
     p_venta = models.DecimalField(max_digits=10, decimal_places=2)
